301dab14257e668c21ceee5d4ecb55f1054d3af0
Mateusz Chmiest <mateusz_269@icloud.com>
2023-03-25 15:01:18 +0100

    basic setup

---
+++ src/components/MainContent/MainContent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideMenu from '../SideMenu/SideMenu'
+
+const MainContent = () => {
+  return (
+    <div>
+        <SideMenu/>
+        MainContent
+    </div>
+  )
+}
+
+export default MainContent
---
+++ src/views/HomePage.tsx
@@ -0,0 +1,15 @@
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
+import MainContent from '../components/MainContent/MainContent'
+
+const HomePage = () => {
+  return (
+    <>
+      <Header/>
+      <MainContent/>
+      <Footer/>
+    </>
+  )
+}
+
+export default HomePage
